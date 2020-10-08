6cb2cef35151c6b63ccf7cad222e92e8340dc2cc
Sindu Andita <sinduandita@gmail.com>
2020-10-08 23:37:59 +0700

    Final refactoring

--- script.js
+++ script.js
@@ -1,96 +1,51 @@
-/*
-let add = document.getElementById("add");
-let input = document.getElementById("input");
-let ul = document.querySelector("ul");
+const addButton = document.getElementById("add");
+const inputBox = document.getElementById("input");
+const todoList = document.querySelector("ul");
+const list = document.querySelectorAll("li");
+const del = document.querySelectorAll(".del");
 
-function inputLength() {
-  return input.value.length;
-}
+const isValidInput = () => inputBox.value !== "";
 
-function createElement() {
-  let li = document.createElement("li");
-  li.appendChild(document.createTextNode(input.value));
-  ul.appendChild(li);
-  input.value = "";
-}
-
-function addListClick() {
-  if (inputLength() > 0) {
-    createElement();
-  }
-}
-
-function addListEnter(e) {
-  if (inputLength() > 0 && e.keyCode === 13) {
-    createElement();
-  }
-}
-
-add.addEventListener("click", addListClick);
-
-input.addEventListener("keypress", addListEnter);
-*/
-
-//Select dom
-let input = document.getElementById("input");
-let add = document.getElementById("add");
-let toDolist = document.querySelector("ul");
-let delButton = document.createElement("button");
-
-//Manipulate dom
-
-// Input new to do ke to do list menggunakan tombol add
-add.addEventListener("click", addByClick);
-
-// Input new to do ke to do list menggunakan enter
-input.addEventListener("keypress", addByPressEnter);
-
-function deleteList() {
-  let del = document.getElementsByClassName("del");
-  for (let i = 0; i < del.length; i++) {
-    del[i].addEventListener("click", function () {
-      this.parentNode.remove();
-    });
-  }
-}
-
-function doneList() {
-  let checked = document.querySelectorAll("li");
-  for (let i = 0; i < checked.length; i++) {
-    checked[i].addEventListener("click", function () {
-      this.classList.toggle("done");
-    });
-  }
-}
-
-function addByClick() {
-  if (input.value !== "") {
-    inject();
-  }
-}
-
-function addByPressEnter(event) {
-  if (input.value !== "" && event.keyCode === 13) {
-    inject();
-  }
-}
-
-function inject() {
-  //Buat element li
-  let newToDo = document.createElement("li");
+const createList = () => {
+  //buat variabel newtodo dan masukkan element li
+  let newTodo = document.createElement("li"); //<li></li>
+  //tambahkan value inputBox kedalam variable newtodo
+  newTodo.appendChild(document.createTextNode(`${inputBox.value} `)); //<li>input</li>
+  //tambahkan newtodo kedalam todoList
   let delButton = document.createElement("button");
   delButton.appendChild(document.createTextNode("x"));
-  delButton.classList.value = "del";
-  //Masukkan text input ke element li
-  newToDo.appendChild(document.createTextNode(input.value));
-  //Masukkan element li ke ul
-  toDolist.appendChild(newToDo);
-  newToDo.appendChild(delButton);
-  //Bersihkan input
-  input.value = "";
-  deleteList();
-  doneList();
+  delButton.addEventListener("click", () => {
+    delButton.parentNode.remove();
+  });
+  newTodo.appendChild(delButton);
+  newTodo.addEventListener("click", () => {
+    newTodo.classList.toggle("done");
+  });
+  todoList.appendChild(newTodo);
+  inputBox.value = "";
+};
+
+const addListByClick = () => {
+  if (isValidInput()) {
+    createList();
+  }
+};
+
+const addListByEnter = (e) => {
+  if (isValidInput() && e.keyCode === 13) {
+    createList();
+  }
+};
+
+for (let i = 0; i < list.length; i++) {
+  list[i].addEventListener("click", () => {
+    list[i].classList.toggle("done");
+  });
+  del[i].addEventListener("click", () => {
+    del[i].parentNode.remove();
+  });
 }
 
-deleteList();
-doneList();
+addButton.addEventListener("click", addListByClick);
+
+inputBox.addEventListener("keypress", addListByEnter);
